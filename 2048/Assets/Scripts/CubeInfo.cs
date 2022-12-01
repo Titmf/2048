b6355e337925de78ec47ea8a51d0613d1ddf0abe
Titmf <mima.miheev.tm@gmail.com>
2022-12-01 18:57:47 +0400

    new generation of cube

--- 2048/Assets/Scripts/CubeInfo.cs
+++ 2048/Assets/Scripts/CubeInfo.cs
@@ -1,11 +1,50 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.Serialization;
 
 public class CubeInfo : MonoBehaviour
 {
+    private Transform _collisionPoint;
+    public int numberOfCube = 2;
+    [SerializeField] private GameObject cube;
     [SerializeField] private Rigidbody cubeRigidbody;
-        
+    
+    //shit code
+    [SerializeField] private TMP_Text text0Number;
+    [SerializeField] private TMP_Text text1Number;
+    [SerializeField] private TMP_Text text2Number;
+    [SerializeField] private TMP_Text text3Number;
+    [SerializeField] private TMP_Text text4Number;
+    [SerializeField] private TMP_Text text5Number;
+    
     private void Start()
     {
-        cubeRigidbody.AddForce(Vector3.forward*1000f);
+        //cubeRigidbody.AddForce(Vector3.forward*1000f);
+        SetNumber();
     }
-}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("cube") && this.name == collision.gameObject.name)
+        {
+            numberOfCube *= 2;
+            _collisionPoint = collision.transform;
+            Destroy(collision.collider.gameObject);
+            cube = Instantiate(cube, _collisionPoint.position, transform.rotation);
+            cube.GetComponent<CubeInfo>().numberOfCube = numberOfCube;
+            cube.name = numberOfCube.ToString();
+            SetNumber();
+        }
+    }
+    
+    //shit code again
+    private void SetNumber()
+    {
+        text0Number.text = numberOfCube.ToString();
+        text1Number.text = numberOfCube.ToString();
+        text2Number.text = numberOfCube.ToString();
+        text3Number.text = numberOfCube.ToString();
+        text4Number.text = numberOfCube.ToString();
+        text5Number.text = numberOfCube.ToString();
+    }
+}        
--- 2048/Assets/Scripts/Fabric.cs
+++ 2048/Assets/Scripts/Fabric.cs
@@ -4,9 +4,25 @@ public class Fabric : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private int[] startNumber = {2,4,8,16,32,64};
+
+    private void Start()
+    {
+        CreateCube(2, spawnPoint);
+        cube.GetComponent<Rigidbody>().useGravity = false;
+    }
 
     private void OnMouseDown()
     {
-        Instantiate(cube, spawnPoint.position, Quaternion.identity);
+        cube.GetComponent<Rigidbody>().useGravity = true;
+        cube.GetComponent<Rigidbody>().AddForce(Vector3.forward*1000f);
+        CreateCube(startNumber[new System.Random().Next(0, startNumber.Length)], spawnPoint);
+    }
+
+    private void CreateCube(int startNumberOfCube, Transform spawnPointOfCube)
+    {
+        cube = Instantiate(cube, spawnPointOfCube.position, transform.rotation);
+        cube.GetComponent<CubeInfo>().numberOfCube = startNumberOfCube;
+        cube.name = startNumberOfCube.ToString();
     }
 }
